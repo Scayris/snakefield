8f75959e4775f0d7c6fc2dbdd530d7c999c1d856
Nishale <jan.makovecki@gmail.com>
2015-11-07 21:39:11 +0100

    add more function skeletons, expand snake object

--- scripts/snakefield.js
+++ scripts/snakefield.js
@@ -7,20 +7,31 @@
  * 
  * 	Global notes:
  * 		All coordinates are given as [Y, X].
- * 		The field begins with [0,0] coordinate in the UPPER LEFT corner.
+ * 		The field begins with [0,0] coordinate in the UPPER LEFT corner.		
+ * 
+ * 		Direction index:
+ * 		+---0---+	This is used for snake and player movement directions. With words:
+ * 		|	↑	|	
+ * 		3 ←	  → 1	0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT.
+ * 		|	↓	|
+ * 		+---2---+
+ * 		
  * 
  */
 
 // Global variables	--------------------------------------------------------------------------------
-var canvas;			// canvas reference
-var gl = null;		// gl context
+var canvas;					// canvas reference
+var gl = null;				// gl context
 
-var entities = [];	// array of snakes (& player)
-var height = 20;	// field height
-var width = 20;		// field width
+var level = 0;				// current game level
+var entities = [];			// array of snakes (& player at position 0)
+var height = 20;			// field height
+var width = 20;				// field width
+var ground;					// field above ground - holds snake positions etc.
+var underground;			// field below ground - holds positions of snakes digging underground
 
-var ground;			// field above ground - holds snake positions etc.
-var underground;	// field below ground - holds positions of snakes digging underground
+var snakePrediction = 3;	// represents the number of turns that snakes plan in advance.
+
 
 // init()	----------------------------------------------------------------------------------------
 //
@@ -49,12 +60,6 @@ function init() {
 	
 	// ::: LOGICS PART ::::
 	
-	// ˇ ENTITY ARRAY ˇ
-	
-	// put player entity in 1st spot of entity array
-	entities.push("player");	//TODO: make an actual player object
-	
-	
 	// ˇ FIELD ˇ
 	
 	// create "ground" and "underground" arrays and initialise them to all zeros
@@ -69,6 +74,12 @@ function init() {
 		}
 	}
 	
+	
+	// ˇ ENTITY ARRAY ˇ
+	
+	// put player entity in 1st spot of entity array
+	entities.push("player");	//TODO: make an actual player object
+	entities.push(makeSnake());	//	The first snake.
 }
 
 /* makeSnake()	------------------------------------------------------------------------------------
@@ -82,35 +93,39 @@ function init() {
  */
  
 function makeSnake() {
-	var snPos = getFreePos();	// Decide the starting position on which snake will begin as [y,x].
-	var snType = 0;				//TODO: decide snake type based on level
-	var snLength = 5;			//TODO: calculate snake length based on level and type + randomisation
-	var snDelay = getDelay();	// Grab delay needed before snake's next move is calculated.
-	var snHead = null;			//TODO: Pointer to snake's model - specifically its head (which points to the next part, etc).
+	var snPos = getFreePos();			// Decide the starting position on which snake will begin as [y,x].
+	var snDir = Math.floor(Math.random()*4);	// Set the snake's movement direction (direction index in global notes)
+	var snType = 0;						//TODO: decide snake type based on current game level
+	var snLength = 5;					//TODO: calculate snake length based on level and type + randomisation
+	var snDelay = getDelay();			// Grab delay needed before snake's next move is calculated.
+	var snHead = genModels(snLength, snDir);	//TODO: Pointer to snake's model - specifically its head (which points to the next part, etc).
+												// ANIMATION FUNCTION ACCESSES THE MODEL THROUGH THIS POINTER.
 	
 	var snake = {
 		pos:snPos,
+		dir:snDir,
 		type:snType,
 		length:snLength,
 		delay:snDelay,
-		head:snHead
-		};
+		head:snHead,
+		planned:0				//how many turns the snake has planned in advance. unless this == snakePrediction
+		};						//	the snake won't move yet - it will plan its moves first.
 	
 	return snake;
 }
 
-/*	getFreePos() -----
+/*	getFreePos() -----																												TODO!!!
  * 
  *		Returns: a free position in the underground array. Used for spawning new snakes.
  */
  
-getFreePos() {
+function getFreePos() {
 	return [0,0]; 
 }
  
  
 
-/* getDelay()	------------------------------------------------------------------------------------		TODO!!!
+/* getDelay()	------------------------------------------------------------------------------------								TODO!!!
  * 
  * Figures out for how long a snake needs to be delayed before its next move is calculated.
  * 
@@ -122,3 +137,17 @@ function getDelay() {
 }
 
 
+/* genModels()	------------------------------------------------------------------------------------								TODO!!!
+ * 
+ * generates the actual 3D model for the given snake. If the tail would fall out of the field at
+ * any point just twist it in any possible direction by 90°.
+ * 
+ * 	Arguments:	length - the length of the snake to be generated
+ * 				dir - the direction that the snake is facing
+ * 	Returns:	reference to an object that holds the details of snake's head. Details include
+ * 				the actual 3D model, texture, 
+ */
+
+function genModels(length, dir) {
+	return null;
+}
